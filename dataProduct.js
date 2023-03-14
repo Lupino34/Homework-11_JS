89522c0e2c0880e9260f59ffcb3344cb8ec1a672
Lupino <vladislav_rozhno@mail.ru>
2023-03-14 19:28:17 +0500

    Homework-11_JS

---
+++ dataProduct.js
@@ -0,0 +1,45 @@
+dataProduct = `
+ [
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "price": "$52.00",
+        "content": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "src": "/img/new img/product1-transformed 1.png",
+        "alt": "product photo"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "price": "$52.00",
+        "content": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "src": "/img/new img/product_article_2-transformed.png",
+        "alt": "product photo"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "price": "$52.00",
+        "content": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "src": "/img/new img/product3-transformed.png",
+        "alt": "product photo"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "price": "$52.00",
+        "content": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "src": "/img/new img/product_article_4-transformed.png",
+        "alt": "product photo"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "price": "$52.00",
+        "content": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "src": "/img/new img/product5-transformed.png",
+        "alt": "product photo"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "price": "$52.00",
+        "content": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "src": "/img/new img/product6-transformed.png",
+        "alt": "product photo"
+    }
+]`
